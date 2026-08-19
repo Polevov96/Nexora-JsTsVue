3e2ddde0281344e7537da25649b964edf9055283
Polevov96 <polevov_96@mail.ru>
2026-08-19 16:37:56 +0300

    style: обновление базовых стилей App и страницы авторизации

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,35 +6,20 @@
 
 <script>
 export default {
- name:"app",
- comments: {
-
- },
-}
+ name: "App",
+};
 </script>
 
 <style>
-
-</style>
-<style>
-
 #app {
-  min-width: 100%8;
   min-height: 100vh;
-  /*  width: 100%;
-  height: 100%; */
-  background-color:#344054;
-  background-image: url("@/assets/fon.png");
-  display: flex;
-  justify-content: flex-start;
-  /* padding: 52px 363px 0px 357px; */
-  margin: 0px;
-  flex-direction: column;
-  align-items: center;
+  min-height: 100dvh;
 }
+
 * {
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
-
 </style>
+
--- frontend/src/views/LoginPage.vue
+++ frontend/src/views/LoginPage.vue
@@ -22,7 +22,7 @@
 
 <script>
 import PageLayout from "@/components/layout/PageLayout.vue";
-import {getUser} from "../api"
+import { getUser } from "../api";
 
 export default {
   name: "LoginPage",
@@ -37,7 +37,6 @@ export default {
   },
   methods: {
     login: async function () {
-      
       // eslint-disable-next-line no-debugger
       if (!this.username) {
         alert("не заполненые данные");
@@ -47,42 +46,74 @@ export default {
         alert("не заполненые данные");
         return;
       } else {
-      //TODO: 1) отправить запрос на сервер
-      const allUsers = await getUser();
-      // console.log(allUsers);
+        //TODO: 1) отправить запрос на сервер
+        const allUsers = await getUser();
+        // console.log(allUsers);
 
-      const result = allUsers.find((user) => {
-        return this.username === user.username;
-      }); 
-      if(result) {
-      this.$store.dispatch("taskModules/addUser", result);
-      this.$router.push("/tasklist");
-      } else {
-        alert("неверно имя пользователя или пароль учетной записи")
+        const result = allUsers.find((user) => {
+          return this.username === user.username;
+        });
+        if (result) {
+          this.$store.dispatch("taskModules/addUser", result);
+          this.$router.push("/tasklist");
+        } else {
+          alert("неверно имя пользователя или пароль учетной записи");
+        }
       }
-    }
-      
-      
+
       // TODO:  проверить на возврат ошибки из api (result instanceof Error)
       // TODO: переписать логику ниже на новые условия
 
-    //    .then((res) => {
-    //       const result = res.data.find((user) => {
-    //         return this.username === user.username;
-    //       });
-    //       console.log(result);
-    //       if (result) {
-    //         this.$store.dispatch("addUser", result);
-    //         // Если true 6
-    //         //TODO: 2) Записать ответ сервера записать в store
-    //         this.$router.push("/tasklist");
-    //       }
-    //       // Если false - вывести ошибку
-    //     })
-    //     .catch((err) => console.log(err));
+      //    .then((res) => {
+      //       const result = res.data.find((user) => {
+      //         return this.username === user.username;
+      //       });
+      //       console.log(result);
+      //       if (result) {
+      //         this.$store.dispatch("addUser", result);
+      //         // Если true 6
+      //         //TODO: 2) Записать ответ сервера записать в store
+      //         this.$router.push("/tasklist");
+      //       }
+      //       // Если false - вывести ошибку
+      //     })
+      //     .catch((err) => console.log(err));
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+.home {
+  min-height: 100vh;
+  min-height: 100dvh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 24px;
+  box-sizing: border-box;
+
+  background: #050505;
+  color: #ffffff;
+}
+
+.login {
+  width: 100%;
+  max-width: 440px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  padding: 32px;
+  box-sizing: border-box;
+
+  background: rgba(12, 12, 12, 0.68);
+
+  border: 1px solid rgba(255, 255, 255, 0.08);
+  border-radius: 24px;
+  backdrop-filter: blur(32px) saturate(120%);
+}
+</style>
